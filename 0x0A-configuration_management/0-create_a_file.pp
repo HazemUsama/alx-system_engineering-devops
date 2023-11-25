ec9692811a62546b53f7e0f36f83b400e6b19460
HazemUsama <hazemosama681@gmail.com>
2023-11-25 04:34:57 +0200

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,6 @@
-# File: /etc/puppet/manifests/school.pp
+#Install Flask
 
-
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
