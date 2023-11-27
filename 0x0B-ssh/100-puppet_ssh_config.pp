ab8be5f01f288bf25826c44040f5ded12b1d0743
HazemUsama <hazemosama681@gmail.com>
2023-11-27 04:31:32 +0200

    done?

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#Client configuration file
+include stdlib
+
+file_line {'refuse to authenticate using a password':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no'
+  replace => true,
+}
+
+file_line {'Add IdentityFile':
+  ensure  => present,
+  paht    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
