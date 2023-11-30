5e20557b864dfb0f7014e30f4c7123d76b197e31
HazemUsama <hazemosama681@gmail.com>
2023-11-30 03:42:23 +0200

    done

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Setup a New Ubuntu server
+
+exec { 'update sys':
+
+    command  => '/usr/bin/apt-get update',
+
+  }
+
+package { 'nginx':
+    ensure   => 'installed', require  => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+    content  => 'Hello World!'
+}
+
+exec {'redirect_me':
+    command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
