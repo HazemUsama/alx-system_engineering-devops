f4283482406eaf312fb0e87cf6d4ce240adc4a5a
HazemUsama <hazemosama681@gmail.com>
2023-11-30 03:47:20 +0200

    outsource

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,25 @@
-# Setup a New Ubuntu server
-
-exec { 'update sys':
-
-    command  => '/usr/bin/apt-get update',
-    provider => shell
-
-  }
+# puppet manifest to configure the web server
+exec { 'dist update':
+        command  => '/usr/bin/apt-get update',
+        provider => 'shell'
+}
 
 package { 'nginx':
-    ensure  => 'installed',
-    require => Exec['update system']
-  }
+  ensure  => 'installed',
+  require => Exec['dist update']
+}
 
 file {'/var/www/html/index.html':
-    content  => 'Hello World!'
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '7624'
 }
 
 exec {'redirect_me':
-    command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell'
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 service {'nginx':
