aec173d1c25faad68ae6aab9e2131b27aaf149e2
HazemUsama <hazemosama681@gmail.com>
2023-12-05 04:41:58 +0200

    done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+#creating a custom HTTP header response
+
+exec { 'command':
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/gzip on;/a\ add_header X-Served-By $hostname;" /etc/nginx/nginx.conf
+  sudo service nginx start'
+  provider => shell,
+  }
