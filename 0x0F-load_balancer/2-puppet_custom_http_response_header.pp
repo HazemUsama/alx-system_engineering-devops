e30ecbddffb732083358597f61089aed54a4a95a
HazemUsama <hazemosama681@gmail.com>
2023-12-05 04:42:44 +0200

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,7 +3,7 @@
 exec { 'command':
   command  => 'sudo apt-get -y update;
   sudo apt-get -y install nginx;
-  sudo sed -i "/gzip on;/a\ add_header X-Served-By $hostname;" /etc/nginx/nginx.conf
+  sudo sed -i "/gzip on;/a\ add_header X-Served-By $hostname;" /etc/nginx/nginx.conf;
   sudo service nginx start'
   provider => shell,
   }
