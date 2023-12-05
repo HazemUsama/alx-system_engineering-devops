209f47deda97b2adf7bfc01ccd4580e227dda7cd
HazemUsama <hazemosama681@gmail.com>
2023-12-05 04:45:02 +0200

    again

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,10 @@
 #creating a custom HTTP header response
-
 exec { 'command':
-  command  => 'sudo apt-get -y update;
-  sudo apt-get -y install nginx;
-  sudo sed -i "/gzip on;/a\ add_header X-Served-By $hostname;" /etc/nginx/nginx.conf;
-  sudo service nginx start'
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i '/gzip on;/a\
+  add_header X-Served-By $hostname;
+  ' /etc/nginx/nginx.conf;
+  service nginx start'
   provider => shell,
   }
