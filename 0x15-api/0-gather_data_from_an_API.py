7e1743df9110157b2c6f92cd7ada8d8544e8db06
HazemUsama <hazemosama681@gmail.com>
2024-02-03 23:55:41 +0200

    Api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Display to-do list given employee ID"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={'userId': sys.argv[1]}).json()
+    done_tasks = len([task for task in todos if task.get('completed')])
+    print('Employee {} is done with tasks({}/{}):'.format(user.get('name'),
+                                                          done_tasks,
+                                                          len(todos)))
+    for task in todos:
+        print('\t {}'.format(task.get('title')))
