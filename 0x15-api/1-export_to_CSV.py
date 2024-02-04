0ff9bfa1d774947720284276e18cf4a3527a511e
HazemUsama <hazemosama681@gmail.com>
2024-02-04 02:49:39 +0200

    CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Display to-do list given employee ID"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={'userId': sys.argv[1]}).json()
+
+    info = [{'id': user.get('id'),
+             'name': user.get('name'),
+             'status': task.get('completed'),
+             'title': task.get('title')} for task in todos]
+
+    with open("{}.csv".format(sys.argv[1]), 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=info[0].keys())
+
+        writer.writerows(info)
