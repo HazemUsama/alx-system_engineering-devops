c0d2c74522a2cb78d5e13f4db72edf93eed60753
HazemUsama <hazemosama681@gmail.com>
2024-02-15 15:00:40 +0200

    Out Source

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Script to show how many subs in a subreddit"""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subs"""
-    results = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit),
-                           headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/Hazemusama)'})
-
-    subs = results.json().get("data", {}).get("subscribers", 0)
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
     return subs
