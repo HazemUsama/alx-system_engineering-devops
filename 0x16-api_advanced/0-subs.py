a2c1d24b4e72101ab26854be9ea602296ea1cf0e
HazemUsama <hazemosama681@gmail.com>
2024-02-15 14:58:23 +0200

    Try this

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subs"""
     results = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit))
+                           .format(subreddit),
+                           headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/Hazemusama)'})
 
     subs = results.json().get("data", {}).get("subscribers", 0)
     return subs
