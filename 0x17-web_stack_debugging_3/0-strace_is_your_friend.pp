38ed9cb10e428989387d88c4fdc9787d65989a6f
HazemUsama <hazemosama681@gmail.com>
2024-02-17 11:20:42 +0200

    Another fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 #Fix typo
 
 exec { 'command':
-  command  => '
-  sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php
-  service apache2 restrat',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   provider => shell,
   }
