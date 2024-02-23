814deefb78fb15c042b40d1f79fa382df3a1a64d
HazemUsama <hazemosama681@gmail.com>
2024-02-23 22:13:51 +0200

    Fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,6 +3,6 @@
 exec { 'command':
   command  => '
   sed -i "s/5/5000/g" /etc/security/limits.conf
-  sed -i "s/4/5000/g" /etc/security/limits.conf
+  sed -i "s/4/5000/g" /etc/security/limits.conf',
   provider => shell,
   }
